c9f4186cfdda9a7fbfcd9648cb788e03a37b1630
Quest <stefan.quest@gmail.com>
2019-08-29 08:34:42 +0200

    addition of graphql and quick test of api with graphiql

--- app.js
+++ app.js
@@ -1,12 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema}= require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req,res, next)=>{
-    res.send('Hello World');
-})
+// for testing express only - delete later
+// app.get('/', (req,res, next)=>{
+//     res.send('Hello World');
+// })
+
+app.use(
+    "/graphql",
+    graphqlHttp({
+      schema: buildSchema(`
+      type RootQuery {
+            items:[String!]!
+      }
+  
+      type RootMutation {
+            createItem(name:String): String
+      }
+  
+      schema{
+          query: RootQuery
+          mutation: RootMutation
+      }
+      `),
+      rootValue: {
+          items: ()=>{
+              return ['black jeans','white t-shirt', 'blue t-shirt'];
+          },
+          createItem: (args)=>{
+              const itemName = args.name;
+              return itemName;
+          }
+      },
+      graphiql:true 
+    })
+  );
+  
 
 app.listen(3001);
